e78196451cc2b8f25aa7c9c15fab92ba31c03207
Kees Huiberts <itissohardtothinkofagoodemail@gmail.com>
2012-05-13 14:11:15 +0200

    WOOOT! It compiles now! But some modifications had to be made to my ndk: lines 41 and 42 in $NDK/platforms/android-4/arch-arm/usr/include/string.h were commented out

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,7 +3,10 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := mp3gain
-LOCAL_SRC_FILES := droidgain.c mp3gain.c gain_analysis.c \
-									 apetag.c id3tag.c rg_error.c
+LOCAL_SRC_FILES := mp3gain.c apetag.c id3tag.c gain_analysis.c rg_error.c \
+	mpglibDBL/common.c mpglibDBL/dct64_i386.c \
+	mpglibDBL/decode_i386.c mpglibDBL/interface.c \
+	mpglibDBL/layer3.c mpglibDBL/tabinit.c \
+	droidgain.c
 
 include $(BUILD_SHARED_LIBRARY)
