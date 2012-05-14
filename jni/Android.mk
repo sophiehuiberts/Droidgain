1d251548e20088a30cf8dc485b7f056c5d506fa9
Kees Huiberts <itissohardtothinkofagoodemail@gmail.com>
2012-05-14 11:57:19 +0200

    JNI is hard, I'll just run an executable instead

--- jni/Android.mk
+++ jni/Android.mk
@@ -6,7 +6,6 @@ LOCAL_MODULE := mp3gain
 LOCAL_SRC_FILES := mp3gain.c apetag.c id3tag.c gain_analysis.c rg_error.c \
 	mpglibDBL/common.c mpglibDBL/dct64_i386.c \
 	mpglibDBL/decode_i386.c mpglibDBL/interface.c \
-	mpglibDBL/layer3.c mpglibDBL/tabinit.c \
-	droidgain.c
+	mpglibDBL/layer3.c mpglibDBL/tabinit.c
 
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_EXECUTABLE)
--- src/org/beide/droidgain/DroidgainActivity.java
+++ src/org/beide/droidgain/DroidgainActivity.java
@@ -1,15 +1,64 @@
 package org.beide.droidgain;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class DroidgainActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+import java.io.IOException;
+import java.lang.Process;
+import java.lang.Runtime;
+
+public class DroidgainActivity extends Activity {
+	
+	String url;
+	LinearLayout column;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		Button BtnPick = new Button(getApplicationContext());
+		BtnPick.setText("Pick");
+		BtnPick.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+				intent.addCategory(Intent.CATEGORY_OPENABLE);
+				intent.setType("audio/mpeg");
+				startActivityForResult(intent, 299792458);
+			}
+		});
+		
+		setContentView(BtnPick);
+	}
+	
+	public void onActivityResult(int request, int result, Intent data) {
+		Uri uri = data.getData();
+		
+		if(uri.getScheme().compareTo("file") == 0) {
+			
+			String schemepart = uri.getSchemeSpecificPart();
+			if(schemepart.substring(0, 2).compareTo("//") == 0) {
+				url = schemepart.substring(2);
+			} else {
+				url = schemepart;
+			}
+			
+			mp3gain(url);
+			
+		}
+	}
+	
+	public void mp3gain(String file) {
+		try {
+			Process process = Runtime.getRuntime().exec(["/sdcard/mp3gain", file]);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
